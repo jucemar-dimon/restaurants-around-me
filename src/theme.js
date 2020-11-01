e6b35178d790d383664a072049e9d6ea7d2dc21d
Jucemar Dimon <jucemar.dimon@gmail.com>
2020-11-01 17:07:28 -0300

    Page Home criada e estilos resetados

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: '#6200ee',
+    background: '#00000014',
+    text: '#0000000b3',
+    success: '#135E01',
+  },
+  fonts: {
+    regular: 'roboto, san-serif',
+  },
+};
+
+export default theme;
